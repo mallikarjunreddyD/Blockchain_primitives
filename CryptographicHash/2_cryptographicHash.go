47b3dc5fd8cca5cf0bfe926831d41814ae2ab64a
Mallikarjun Reddy <mallikarjun.reddy@mckinleyrice.co>
2022-12-28 17:46:01 +0530

    add cryptographicHash.go

---
+++ CryptographicHash/2_cryptographicHash.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"crypto/md5"
+	"crypto/sha256"
+	"fmt"
+
+	"github.com/wealdtech/go-merkletree/keccak256"
+)
+
+func main() {
+	fmt.Println(getHash("John smith"))
+	fmt.Println(getHash("Lisa Smith"))
+	fmt.Println(getHash("Sam Doe"))
+	fmt.Println(getHash("Sandra Dee"))
+}
+func getHash(data string) string {
+	result := fmt.Sprintf("MD5: %x, SHA256: %x, Keccak256: %x",
+		md5.Sum([]byte(data)),
+		sha256.Sum256([]byte(data)), keccak256.New().Hash([]byte(data)))
+	return result
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/mallikarjunreddyD/Blockchain_primitives
 
 go 1.17
+
+require (
+	github.com/wealdtech/go-merkletree v1.0.0 // indirect
+	golang.org/x/crypto v0.4.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+)
